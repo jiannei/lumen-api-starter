29b1a300b7412aafa69a1fb678fdd41007c912ec
jiannei <longjian.huang@aliyun.com>
2020-07-29 17:40:20 +0800

    fix: 枚举转换出现问题(#24)

--- resources/lang/en/enums.php
+++ resources/lang/en/enums.php
@@ -10,11 +10,34 @@
  */
 
 use App\Repositories\Enums\ExampleEnum;
+use App\Repositories\Enums\ResponseCodeEnum;
 
 return [
     // example
     ExampleEnum::class => [
         ExampleEnum::ADMINISTRATOR => 'Administrator',
         ExampleEnum::SUPER_ADMINISTRATOR => 'Super administrator',
+    ],
+
+    ResponseCodeEnum::class => [
+        // 成功
+        ResponseCodeEnum::HTTP_OK => 'Success.', // 自定义 HTTP 状态码返回消息
+
+        // 业务操作成功
+        ResponseCodeEnum::SERVICE_REGISTER_SUCCESS => 'Register success.',
+        ResponseCodeEnum::SERVICE_LOGIN_SUCCESS => 'Login success.',
+
+        // 客户端错误
+        ResponseCodeEnum::CLIENT_PARAMETER_ERROR => 'Parameter error.',
+        ResponseCodeEnum::CLIENT_CREATED_ERROR => 'Created error.',
+        ResponseCodeEnum::CLIENT_DELETED_ERROR => 'Deleted error.',
+
+        // 服务端错误
+        ResponseCodeEnum::SYSTEM_ERROR => 'System error.',
+        ResponseCodeEnum::SYSTEM_UNAVAILABLE => 'System unavailable.',
+
+        // 业务操作失败：授权业务
+        ResponseCodeEnum::SERVICE_REGISTER_ERROR => 'Register error.',
+        ResponseCodeEnum::SERVICE_LOGIN_ERROR => 'Login error,',
     ],
 ];
