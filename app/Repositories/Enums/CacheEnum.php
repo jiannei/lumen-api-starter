5c823db745790605512c431e3e2dc19d8cff1e05
jiannei <longjian.huang@aliyun.com>
2020-10-14 20:09:07 +0800

    pref: 规范缓存 Key 的定义和使用

---
+++ app/Repositories/Enums/CacheEnum.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Repositories\Enums;
+
+
+use App\Support\Enum\Enum;
+
+class CacheEnum extends Enum
+{
+    // 定义缓存时用到的 key 值；以冒号区分层级
+    const AUTHORIZATION_USER = 'authorization:user';
+
+    /**
+     * 根据生成缓存时的 index
+     *
+     * @param  string|Enum|integer  $cacheEnumKeyOrValue  缓存常量的 key 值或 value 值
+     * @param  string|null  $identifier  integer|string 缓存区分
+     * @return string
+     */
+    public static function buildKey($cacheEnumKeyOrValue, ?string $identifier = null)
+    {
+        $key = (string) self::make($cacheEnumKeyOrValue);
+
+        return is_null($identifier) ? $key : $key.':'.$identifier;
+    }
+}
