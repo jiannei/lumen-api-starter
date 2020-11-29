0f3c646d9c56c8a3a1e393613c186e6bca953a47
Jiannei <Jiannei@users.noreply.github.com>
2020-11-29 15:02:31 +0000

    Apply fixes from StyleCI

--- config/response.php
+++ config/response.php
@@ -1,7 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Jiannei/lumen-api-starter.
+ *
+ * (c) Jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use App\Repositories\Enums\ResponseCodeEnum;
 
 return [
-    'enum' => ResponseCodeEnum::class
+    'enum' => ResponseCodeEnum::class,
 ];
