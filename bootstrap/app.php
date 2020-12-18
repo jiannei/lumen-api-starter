206a82c5eebd004d960ae0a9abe58d0747829ac9
jiannei <longjian.huang@aliyun.com>
2020-12-18 12:07:49 +0800

    fix: 修复依赖引入问题

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -139,7 +139,7 @@ $app->register(App\Providers\EventServiceProvider::class);
 $app->register(\Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(\Illuminate\Redis\RedisServiceProvider::class);
 $app->register(\Spatie\Permission\PermissionServiceProvider::class);
-$app->register(\Jiannei\Enum\Laravel\Providers\ServiceProvider::class);
+$app->register(\Jiannei\Enum\Laravel\Providers\LumenServiceProvider::class);
 $app->register(\Jiannei\Response\Laravel\Providers\LumenServiceProvider::class);
 $app->register(\Jiannei\Logger\Laravel\Providers\ServiceProvider::class);
 
