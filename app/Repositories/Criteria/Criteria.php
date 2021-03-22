d4cfefd2481426884fdeaf418a927ee92b364328
jiannei <longjian.huang@aliyun.com>
2021-03-22 10:48:55 +0800

    pref: laravel-response & base criteria

--- app/Repositories/Criteria/Criteria.php
+++ app/Repositories/Criteria/Criteria.php
@@ -19,7 +19,7 @@ use Prettus\Repository\Contracts\RepositoryInterface;
 abstract class Criteria implements CriteriaInterface
 {
     /**
-     * @var \Illuminate\Http\Request
+     * @var Request
      */
     protected $request;
 
@@ -30,26 +30,33 @@ abstract class Criteria implements CriteriaInterface
 
     public function apply($model, RepositoryInterface $repository)
     {
+        $model = $this->before($model);
+
         $model = $model->where(function ($query) {
             /** @var Builder $query */
-            $this->defaultCondition($query);
-
             $this->condition($query);
         });
 
+        return $this->after($model);
+    }
+
+    /**
+     * Splice the query according to the parameters in the request
+     *
+     * @param  Builder  $query
+     */
+    protected function condition(Builder $query): void
+    {
+
+    }
+
+    protected function before($model)
+    {
         return $model;
     }
 
-    protected function defaultCondition(Builder $query, $nullValue = 0): void
+    protected function after($model)
     {
-        // 默认的筛选条件，防止返回数据过多（没有传任何参数时返回空数据）
-        if (! $this->request->all()) {
-            $query->where($query->getModel()->getKeyName(), $nullValue);
-        }
-    }
-
-    protected function condition(Builder $query): void
-    {
-        // 根据 request 中的参数拼接 query
+        return $model;
     }
 }
--- config/response.php
+++ config/response.php
@@ -12,13 +12,55 @@
 use App\Repositories\Enums\ResponseCodeEnum;
 
 return [
-    'enum' => ResponseCodeEnum::class,
+    /*
+   |--------------------------------------------------------------------------
+   | Set the http status code when the response fails
+   |--------------------------------------------------------------------------
+   |
+   | the reference options are false, 200, 500
+   |
+   | false, stricter http status codes such as 404, 401, 403, 500, etc. will be returned
+   | 200, All failed responses will also return a 200 status code
+   | 500, All failed responses return a 500 status code
+   */
+
+    'error_code' => false,
+
+    // Set the http status code returned when the form validation fails.
+    //  When the error_code is set to 200 or 500, it will not work
 
     'validation_error_code' => ResponseCodeEnum::CLIENT_VALIDATION_ERROR,
+
+    // Set the structure of the paging data return,the following structure will be returned by default,
+    // You can modify the name of the inner data field through the following configuration items, such as rows or list
+    //{
+    //    "status": "success",
+    //    "code": 200,
+    //    "message": "Success.",
+    //    "data": {
+    //    "data": [
+    //        // ...
+    //    ],
+    //        "meta": {
+    //        // ...
+    //    }
+    //    },
+    //    "error": {}
+    //}
 
     'format' => [
         'paginated_resource' => [
             'data_field' => 'list',
         ],
     ],
+
+    // You can use enumerations to define the code when the response is returned,
+    // and set the response message according to the locale
+    //
+    // The following two enumeration packages are good choices
+    //
+    // https://github.com/Jiannei/laravel-enum
+    // https://github.com/BenSampo/laravel-enum
+
+     'enum' => ResponseCodeEnum::class,
 ];
